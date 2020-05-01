bc680246169488f13b4f0a2e67f82ca53181bc4e
Riku-Juhani Niemelä <rjniem@gmail.com>
2020-05-01 19:27:05 +0300

    Added Webpack, babel and basic react setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+function App(props) {
+    return (
+        <div>
+            <h1 style={{fontSize: '120px'}}>Hello</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
