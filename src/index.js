5772e55b3f557f35eaf3a9454d1b356e4a4599e1
Riku-Juhani Niemelä <rjniem@gmail.com>
2020-05-01 19:45:45 +0300

    Changes, changes, changes everywhere

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 function App(props) {
-    return (
-        <div>
-            <h1 style={{fontSize: '120px'}}>Hello</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1 style={{ fontSize: "120px" }}>Hello</h1>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
---
+++ src/redux/SectionSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const sectionSlice = createSlice({
+    name: 'section',
+    initialState: [],
+    reducers: {
+        addSection(state, action) {
+            console.log(action.payload);
+        }
+    }
+});
+
+export const {
+    addSection
+} = sectionSlice.actions;
+
+export default sectionSlice.reducer;
